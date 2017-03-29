913db934a93f463bc03a04d629b5590687e738b9
datcomputerguy <datcomputerguy@gmail.com>
2017-03-29 15:45:23 -0500

    Add files via upload

---
+++ 20170325-CBU-Lab/auto.sh
@@ -0,0 +1,38 @@
+username=$1
+password=$2
+organization=$3
+space=$4
+appname=$5
+
+echo $user
+
+echo "" > manifest.yml
+echo "applications:" >> manifest.yml
+echo "- path: ." >> manifest.yml
+echo "  memory: 512M" >> manifest.yml
+echo "  instances: 1" >> manifest.yml
+echo "  domain: mybluemix.net" >> manifest.yml
+echo "  name: $appname" >> manifest.yml
+echo "  host: $appname" >> manifest.yml
+echo "  disk_quota: 1024M" >> manifest.yml
+echo "  services:" >> manifest.yml
+echo "  - $appname-cloudantNoSQLDB" >> manifest.yml
+
+cf l -u $username -p $password
+
+cf t -o $organization -s $space
+
+cf cs conversation free conversation
+cf bs $appname conversation
+
+cf cs text_to_speech standard text-to-speech
+cf bs $appname text-to-speech
+
+cf cs speech_to_text standard speech-to-text
+cf bs $appname speech-to-text
+
+cf cs tone_analyzer standard tone-analyzer
+cf bs $appname tone-analyzer
+
+cf push $appname
+
